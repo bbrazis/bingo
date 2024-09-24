68b87f2ff7a7f4239f27e6bcb12fb790b4d26786
bbrazis <32001385+bbrazis@users.noreply.github.com>
2024-09-24 09:56:17 -0700

    Update Nav.jsx

--- components/Nav/Nav.jsx
+++ components/Nav/Nav.jsx
@@ -4,14 +4,14 @@ import { Link, NavLink } from 'react-router-dom'
 export default function Nav() {
     return (
         <>
-            <div className='nav-wrap'>
+            <header className='nav-wrap'>
                 <nav>
                     <ul>
                         <li><NavLink to="/">Bingo</NavLink></li>
                         <li><NavLink to="suggestions" >Suggestions</NavLink></li>
                     </ul>
                 </nav>
-            </div>
+            </header>
         </>
     )
 }
