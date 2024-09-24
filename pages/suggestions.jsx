3d059ce2fda120c125dc4e670f9b3b11790234ba
bbrazis <32001385+bbrazis@users.noreply.github.com>
2024-09-24 16:14:07 -0700

    Update suggestions.jsx

--- pages/suggestions.jsx
+++ pages/suggestions.jsx
@@ -3,13 +3,38 @@ import Nav from '../components/Nav/Nav'
 
 
 export default function Suggestions() {
+    const [formData, setFormData] = React.useState({ terms: "" })
+    const [status, setStatus] = React.useState('idle')
+    const [error, setError] = React.useState(null)
+
+    function handleSubmit (e){
+        e.preventDefault()
+        setStatus('submitting')
+
+    }
+
+    function handleChange(e) {
+        const { name, value } = e.target
+        setFormData(prev => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
     return (
         <>
             <Nav />
             <h1>Bingo Suggestion Page</h1>
-            <form>
+            <form onSubmit={handleSubmit} className='new-terms-form'>
                 <label>Bingo Phrase
-                    <input type='text' id='phrase-input' />
+                    <input 
+                        name='terms'
+                        onChange={handleChange}
+                        id='terms'
+                        type='text'
+                        placeholder='new phrase to be added to the database'
+                        value={formData.terms}
+                    />
                 </label>
                 <button type='submit'>Submit</button>
             </form>
