1e6b94a27edffbd9c71146a9e73cf762f94e9f6b
bbrazis <32001385+bbrazis@users.noreply.github.com>
2024-09-24 09:49:41 -0700

    Update Home.jsx

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react"
 import Bingo from "../components/Bingo"
 import Nav from "../components/Nav/Nav"
+import data from "../data"
 
 export default function Home() {
     const values = Object.values(data)
-  const Squares = values.map((value,index) => <Bingo.Square key={index} text={value} />)
+    const Squares = values.map((value,index) => <Bingo.Square key={index} text={value} />)
     return (
         <>
             <h1>Kid Bingo</h1>
