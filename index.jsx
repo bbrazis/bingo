2f83b1bd772b5e0c854d21a6dec372ca717dcf40
bbrazis <32001385+bbrazis@users.noreply.github.com>
2024-09-24 08:38:21 -0700

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -15,6 +15,14 @@ function App() {
         {Squares}
       </Bingo>
       <button className="reset-btn" onClick={() => location. reload()}>Restart</button>
+      <div className='nav-wrap'>
+        <nav>
+          <ul>
+            <li><a href='/'>Bingo</a></li>
+            <li><a href='./suggestions'>Suggestions</a></li>
+          </ul>
+        </nav>
+      </div>
     </>
   )
 }
