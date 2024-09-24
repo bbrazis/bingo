6868716056ac700b173a9239360a76e96a527f6b
bbrazis <32001385+bbrazis@users.noreply.github.com>
2024-09-24 12:58:40 -0700

    Update data.js

--- data.js
+++ data.js
@@ -26,5 +26,43 @@ const options =
         24: "Kid gets injured",
         25: "James runs off without notice"
     }
-    
+
+const shuffle = (arr) => {
+    return arr.map(value => ({value, sort: Math.random( )}))
+        .sort((a, b) => a.sort - b.sort)
+        .map(({value})=> value)
+}
+
+
+const getFullList = async () => {
+    try {
+        const res = await fetch('https://x8ki-letl-twmt.n7.xano.io/api:OJ5FJ_Jw/bingo')
+        const data = await res.json()
+
+        return await data
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+const getRandomList = async () => {
+    const data = await getFullList()
+    const randomList = await shuffle(data)
+    let newArr = []
+
+    for(let i = 0; i < 25; i++) {
+        const values = Object.values(randomList)
+        if(i < 13) {
+            newArr.push(values[i])
+        } else if (i === 13){
+            newArr.push("FREE SPACE")
+        } else {
+            newArr.push(values[i])
+        }
+    }
+
+    return newArr
+}
 export default options
+
+export { getFullList, getRandomList }
