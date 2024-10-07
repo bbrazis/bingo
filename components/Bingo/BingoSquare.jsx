21faa086c8057f578c9f6a4e02027d9ef524143f
bbrazis <32001385+bbrazis@users.noreply.github.com>
2024-10-07 14:20:36 -0700

    Update BingoSquare.jsx

--- components/Bingo/BingoSquare.jsx
+++ components/Bingo/BingoSquare.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
 
 export default function BingoSquare({text, checked, toggleSquare}){
-    // const [selected, setSelected] = React.useState(false)
-    
-    // function toggleSquare(){
-    //     setSelected(prev => !prev)
-    // }
-    
     return (
         <div className={`square${checked?" selected":""}`}>
-            {text ? text : "Free Space"}
+            {text}
             <button className="clickable" onClick={toggleSquare}>
-                <p className="visually-hidden">{text ? text : "Free Space"}</p>
+                <p className="visually-hidden">{text}</p>
             </button>
         </div>
     )
