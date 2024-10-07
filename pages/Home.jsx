3af77a38f9512e69303df4e7393b8c92e351b4eb
bbrazis <32001385+bbrazis@users.noreply.github.com>
2024-10-07 14:09:41 -0700

    Update Home.jsx

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -43,7 +43,7 @@ export default function Home() {
                 return item
             }
         })
-        console.log(mappedArr)
+        setBingoItems(mappedArr)
     }
 
     if(loading) {
