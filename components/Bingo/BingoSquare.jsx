a050fcee3fe7f18f7e46bf81e9c351ea9cf9e3ac
bbrazis <32001385+bbrazis@users.noreply.github.com>
2024-10-07 13:12:09 -0700

    Update BingoSquare.jsx

--- components/Bingo/BingoSquare.jsx
+++ components/Bingo/BingoSquare.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function BingoSquare({text, checked}){
+export default function BingoSquare({text, checked, toggleSquare}){
     // const [selected, setSelected] = React.useState(false)
     
     // function toggleSquare(){
