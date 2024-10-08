dafe7ba3ec066afdd866a147be6092615e00067e
bbrazis <32001385+bbrazis@users.noreply.github.com>
2024-10-08 10:12:01 -0700

    Update data.js

--- data.js
+++ data.js
@@ -1,31 +1,17 @@
-const options = 
-    {
-        1: "Poppy's Playtime",
-        2: "Among Us",
-        3: "Lava Monster",
-        4: "NOOOO",
-        5: "I want a creamer",
-        6: "I need water",
-        7: "TRIN / TRINITY",
-        8: "LONDON",
-        9: "SASHA",
-        10: "HEEY!",
-        11: "DEVIN!",
-        12: "GRACIE!",
-        13: "FREE SPACE",
-        14: "Let's go to the house",
-        15: "I'll race you",
-        16: "I want a rolly rancher",
-        17: "Can I get a hot chocolate",
-        18: "Screams of some sort",
-        19: "Going into the pool area",
-        20: "Can I have ____ snack?",
-        21: "among among us",
-        22: "Can we see the kitty?",
-        23: "Kid breakdown or tantrum",
-        24: "Kid gets injured",
-        25: "James runs off without notice"
-    }
+const winConditions = [
+    [0,1,2,3,4],
+    [0,5,10,15,20],
+    [0,6,12,18,24],
+    [1,6,11,16,21],
+    [2,7,12,17,22],
+    [3,8,13,18,23],
+    [4,9,14,19,24],
+    [5,6,7,8,9],
+    [10,11,12,13,14],
+    [15,16,17,18,19],
+    [20,21,22,23,24],
+    [20,16,12,8,4]
+]
 
 const shuffle = (arr) => {
     return arr.map(value => ({value, sort: Math.random( )}))
@@ -91,6 +77,6 @@ const sendData = async (formData) => {
     return `'${data.terms}' was successfully added!`
 }
 
-export default options
+export default winConditions
 
 export { getFullList, getRandomList, sendData }
