0f1950d0424dde8ddb086d4f8c4f141e7b2af153
bbrazis <32001385+bbrazis@users.noreply.github.com>
2024-10-09 10:10:42 -0700

    Update Home.jsx

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -56,6 +56,7 @@ export default function Home() {
             }
 
             setBingoItems( newArr )
+            setWinning(false)
         } catch (err) {
             console.error(err)
         } finally {
